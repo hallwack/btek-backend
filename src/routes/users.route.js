3cb72980d6f990a650f8124fea8d55dc25576c87
Raihan Adam <hallwack.id@gmail.com>
2022-10-17 01:26:54 +0700

    feat: add validation middleware in routes (middleware solved)

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -7,9 +7,9 @@ const {
 } = require("../middlewares/validation.middleware");
 
 users.get("/", userController.readAllUsers);
-users.post("/", userValidation(), validate, userController.createUser);
+users.post("/", userValidation, validate, userController.createUser);
 users.get("/:id", userController.readUserById);
-users.put("/:id", userValidation(), validate, userController.updateUserById);
+users.put("/:id", userValidation, validate, userController.updateUserById);
 users.delete("/:id", userController.deleteUserById);
 
 module.exports = users;
