07facc2dd287c8280b12a958d5664c0a227ccf9c
Raihan Adam <hallwack.id@gmail.com>
2022-10-17 01:26:21 +0700

    fix: fixing parameter in validate func

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -1,27 +1,23 @@
 const { check, validationResult } = require("express-validator");
 
-const userValidation = () => {
-  return [
-    check("email").isEmail().withMessage("Provide valid email"),
-    check("password")
-      .isLength({ min: 8 })
-      .withMessage("Password must be at least 8 character"),
-  ];
-};
+const userValidation = [
+  check("email")
+    .normalizeEmail()
+    .isEmail()
+    .notEmpty()
+    .withMessage("Provide valid email"),
+  check("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 character")
+    .notEmpty()
+    .withMessage("Password is missing"),
+];
 
-const validate = (res, req, next) => {
-  const error = validationResult(req);
-  if (error.isEmpty()) return next();
-
-  const extractedErrors = [];
-  error.array().map((err) =>
-    extractedErrors.push({
-      [err.param]: err.msg,
-    })
-  );
-
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) return next();
   return res.status(422).json({
-    error: extractedErrors,
+    errors: errors.array(),
   });
 };
 
