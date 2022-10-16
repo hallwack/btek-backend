4c9de27443b4dfd2de63e9d5aecb7336ab1e5786
Raihan Adam <hallwack.id@gmail.com>
2022-10-16 19:46:45 +0700

    feat: add middleware into users post and put method (unsolved)

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,12 +1,15 @@
 const users = require("express").Router();
 
 const userController = require("../controllers/users.controller");
-const checkMiddleware = require("../middlewares/check.middleware")
+const {
+  userValidation,
+  validate,
+} = require("../middlewares/validation.middleware");
 
 users.get("/", userController.readAllUsers);
-users.post("/", userController.createUser);
+users.post("/", userValidation(), validate, userController.createUser);
 users.get("/:id", userController.readUserById);
-users.put("/:id", userController.updateUserById);
+users.put("/:id", userValidation(), validate, userController.updateUserById);
 users.delete("/:id", userController.deleteUserById);
 
 module.exports = users;
