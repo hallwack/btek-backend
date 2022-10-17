09bcc297ea6a711d8871752dbe490f0da12ac909
Raihan Adam <hallwack.id@gmail.com>
2022-10-17 20:56:11 +0700

    feat: add params validation

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -4,12 +4,19 @@ const userController = require("../controllers/users.controller");
 const {
   userValidator,
   validate,
+  paramsUUID,
 } = require("../middlewares/validation.middleware");
 
 users.get("/", userController.readAllUsers);
 users.post("/", userValidator, validate, userController.createUser);
-users.get("/:id", userController.readUserById);
-users.put("/:id", userValidator, validate, userController.updateUserById);
-users.delete("/:id", userController.deleteUserById);
+users.get("/:id", paramsUUID, validate, userController.readUserById);
+users.put(
+  "/:id",
+  paramsUUID,
+  userValidator,
+  validate,
+  userController.updateUserById
+);
+users.delete("/:id", paramsUUID, validate, userController.deleteUserById);
 
 module.exports = users;
