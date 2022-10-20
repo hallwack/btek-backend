3c451e60aa10aecadc91527b9406364ce6b92df4
Raihan Adam <hallwack.id@gmail.com>
2022-10-20 12:34:55 +0700

    feat: add profile module (route, model, and controller)

---
+++ src/controllers/profile.controller.js
@@ -0,0 +1,48 @@
+const profileModel = require("../models/profile.model");
+
+exports.readProfileById = async (req, res) => {
+  try {
+    const profile = await profileModel.selectProfileByUserId(
+      req.params.id || req.userData.id
+    );
+    if (profile.rowCount) {
+      return res.json({
+        success: true,
+        message: "Profile user with id" + req.params.id,
+        results: profile.rows[0],
+      });
+    }
+    return res.status(400).json({
+      success: false,
+      message:
+        "User with id " + req.params.id || req.userData.id + " not found!",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: `Error: ${err.message}`,
+    });
+  }
+};
+
+exports.updateProfile = async (req, res) => {
+  try {
+    const profile = await profileModel.updateProfileByUserId(
+      req.userData.id,
+      req.body
+    );
+
+    if (profile.rowCount) {
+      return res.json({
+        success: true,
+        message: "Update profile success",
+        results: profile.rows[0],
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: `Error: ${err.message}`,
+    });
+  }
+};
---
+++ src/models/profile.model.js
@@ -0,0 +1,32 @@
+const db = require("../helpers/db");
+
+const table = "profile";
+
+exports.insertProfile = (data) => {
+  const sql = `INSERT INTO ${table} ("fullname", "picture", "birthDate", "userId") VALUES ($1, $2, $3, $4) RETURNING *`;
+  const params = [data.fullname, data.picture, data.birthDate, data.userId];
+  return db.query(sql, params);
+};
+
+exports.selectProfileByUserId = (id) => {
+  const sql = `SELECT * FROM ${table} WHERE "userId" = $1`;
+  const params = [id];
+  return db.query(sql, params);
+};
+
+exports.updateProfileByUserId = (id, data) => {
+  const column = Object.keys(data);
+  const values = Object.values(data);
+
+  const conditionalSql = [];
+
+  column.forEach((col, index) => {
+    conditionalSql.push(`"${col}"=$${2 + index}`);
+  });
+
+  const sql = `UPDATE "${table}" SET ${conditionalSql.join(
+    ", "
+  )} WHERE "userId" = $1 RETURNING *`;
+  const params = [id, ...values];
+  return db.query(sql, params);
+};
