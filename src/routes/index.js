67fe2da037f8373870d50c84d67c05b44158e647
Raihan Adam <hallwack.id@gmail.com>
2022-10-20 12:37:00 +0700

    feat: add auth and profile to route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,9 @@
 const routes = require("express").Router();
 
+const authMiddleware = require("../middlewares/auth.middleware");
+
 routes.use("/users", require("../routes/users.route"));
+routes.use("/auth", require("../routes/auth.route"));
+routes.use("/profile", authMiddleware, require("../routes/profile.route"));
 
 module.exports = routes;
