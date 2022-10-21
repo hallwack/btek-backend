b2ace778e89d7171e46faec593882ce22b4a206a
Raihan Adam <hallwack.id@gmail.com>
2022-10-22 04:02:54 +0700

    fix: change params data

--- src/models/forgotPassword.model.js
+++ src/models/forgotPassword.model.js
@@ -2,21 +2,21 @@ const db = require("../helpers/db");
 
 const table = "forgotPassword";
 
-exports.forget = (code, email, userId) => {
+exports.forget = (data) => {
   const sql = `INSERT INTO "${table}" ("code", "email", "userId") VALUES ($1, $2, $3) RETURNING *`;
-  const params = [code, email, userId];
+  const params = [data.code, data.email, data.userId];
   return db.query(sql, params);
 };
 
 exports.resetPassword = (email, password) => {
-  const sql = "UPDATE \"users\" SET password = $2 WHERE email = $1 RETURNING *";
+  const sql = 'UPDATE "users" SET password = $2 WHERE email = $1 RETURNING *';
   const params = [email, password];
   return db.query(sql, params);
 };
 
-exports.getForgotPassword = (code) => {
-  const sql = `SELECT * FROM "${table}" WHERE "code" = $1`;
-  const params = [code];
+exports.getForgotPassword = (data) => {
+  const sql = `SELECT * FROM "${table}" WHERE "code" = $1 AND email = $2`;
+  const params = [data.code, data.email];
   return db.query(sql, params);
 };
 
