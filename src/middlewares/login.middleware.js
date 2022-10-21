ccb0b9858e4431ef6a325e45e138308a87475cc3
Raihan Adam <hallwack.id@gmail.com>
2022-10-22 00:24:24 +0700

    fix: delete comment

--- src/middlewares/login.middleware.js
+++ src/middlewares/login.middleware.js
@@ -7,7 +7,7 @@ exports.login = [
     .withMessage("Provide valid email")
     .notEmpty()
     .withMessage("Email must be input"),
-  /* body("password")
+  body("password")
     .isStrongPassword({
       minLength: 8,
       minSymbols: 1,
@@ -18,5 +18,5 @@ exports.login = [
       "Password must be at least 8 characters or more, including 1 symbol, 1 uppercase, and 1 number"
     )
     .notEmpty()
-    .withMessage("Password is missing"), */
+    .withMessage("Password is missing"),
 ];
