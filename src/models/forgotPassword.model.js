e9843acb3720c3946bbd38a086c580cc4ab5869a
Raihan Adam <hallwack.id@gmail.com>
2022-10-21 19:52:31 +0700

    feat: add updateForgotPassword model query

--- src/models/forgotPassword.model.js
+++ src/models/forgotPassword.model.js
@@ -9,7 +9,7 @@ exports.forget = (code, email, userId) => {
 };
 
 exports.resetPassword = (email, password) => {
-  const sql = "UPDATE \"users\" SET password = $2 WHERE email = $1 RETURNING *";
+  const sql = 'UPDATE "users" SET password = $2 WHERE email = $1 RETURNING *';
   const params = [email, password];
   return db.query(sql, params);
 };
@@ -17,5 +17,11 @@ exports.resetPassword = (email, password) => {
 exports.getForgotPassword = (code) => {
   const sql = `SELECT * FROM "${table}" WHERE "code" = $1`;
   const params = [code];
+  return db.query(sql, params);
+};
+
+exports.updateForgotPassword = (id, email) => {
+  const sql = `UPDATE "${table}" SET email = $2 WHERE "userId" = $1 RETURNING *`;
+  const params = [id, email];
   return db.query(sql, params);
 };
