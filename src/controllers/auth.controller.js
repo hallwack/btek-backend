c37be12fec80edf2fbd91f526da6b0f973972f04
Raihan Adam <hallwack.id@gmail.com>
2022-10-22 04:01:45 +0700

    fix: change forgotPassword and resetPassword to authController

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,19 @@
 const argon = require("argon2");
 const userModel = require("../models/users.model");
+const forgotPasswordModel = require("../models/forgotPassword.model");
 const jwt = require("jsonwebtoken");
 const { insertProfile } = require("../models/profile.model");
+
+const generateCode = () => {
+  const digits = "0123456789";
+  let code = "";
+
+  for (let i = 0; i < 6; i++) {
+    code += digits[Math.floor(Math.random() * 10)];
+  }
+
+  return code;
+};
 
 exports.login = async (req, res) => {
   try {
@@ -41,16 +53,96 @@ exports.register = async (req, res) => {
   try {
     req.body.password = await argon.hash(req.body.password);
     const user = await userModel.insertUser(req.body);
+
     if (user.rowCount) {
       const createdUser = user.rows[0];
       req.body.userId = createdUser.id;
       const profile = await insertProfile(req.body);
+
       if (profile.rowCount) {
         return res.json({
           success: true,
           message: "Register Success",
         });
       }
+    }
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: `Error: ${err.message}`,
+    });
+  }
+};
+
+exports.forgotPassword = async (req, res) => {
+  try {
+    req.body.code = generateCode();
+    const user = await userModel.selectUserByEmail(req.body.email);
+    req.body.userId = user.rows[0].id;
+    if (user.rowCount) {
+      const forgotPassword = await forgotPasswordModel.forget(req.body);
+
+      if (forgotPassword.rowCount) {
+        return res.json({
+          success: true,
+          message: "Create new password succesfully",
+          results: forgotPassword.rows[0],
+        });
+      }
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: `Email not found!`,
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: `Error: ${err.message}`,
+    });
+  }
+};
+
+exports.resetPassword = async (req, res) => {
+  try {
+    const user = await forgotPasswordModel.getForgotPassword(req.body);
+
+    if (user.rowCount) {
+      if (user.rows[0].updatedAt) {
+        return res.status(400).json({
+          success: false,
+          message: "Code has been used",
+        });
+      }
+
+      const updatePassword = await userModel.updateUserById(
+        user.rows[0].userId,
+        user.rows[0].email,
+        await argon.hash(req.body.newPassword)
+      );
+
+      if (updatePassword.rowCount) {
+        const updatePassword = await forgotPasswordModel.updateForgotPassword(
+          user.rows[0].userId,
+          user.rows[0].email
+        );
+
+        return res.json({
+          success: true,
+          message: "Reset password successfully!",
+          results: updatePassword.rows[0],
+        });
+      }
+
+      return res.status(500).json({
+        success: false,
+        message: "Unexpected error on updating data",
+      });
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: `Email or code cannot be identified!`,
+      });
     }
   } catch (err) {
     return res.status(500).json({
