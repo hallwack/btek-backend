2dd6794d4bf087717f163b3ce96206850f3937a8
Raihan Adam <hallwack.id@gmail.com>
2022-10-21 18:45:37 +0700

    feat: fixing forgotPassword structure and add resetPassword method

--- src/controllers/passwordSettings.controller.js
+++ src/controllers/passwordSettings.controller.js
@@ -1,26 +1,83 @@
 const argon = require("argon2");
 const forgotPasswordModel = require("../models/forgotPassword.model");
+const userModel = require("../models/users.model");
+
+const generateCode = () => {
+  const digits = "0123456789";
+  let code = "";
+
+  for (let i = 0; i < 6; i++) {
+    code += digits[Math.floor(Math.random() * 10)];
+  }
+
+  return code;
+};
 
 exports.forgotPassword = async (req, res) => {
   try {
     // nerima email
     // create forgotPassword ke tabel password
     // passing code baru
+
     const { email } = req.body;
-    const id = req.userData;
-    const password = `forgotPassword-${Date.now().toString()}`;
-    const hashPassword = await argon.hash(password);
+    const getUserByEmail = await userModel.selectUserByEmail(email);
+    const id = getUserByEmail.rows[0].id;
+    /* console.log(id) */
 
     const forgotPassword = await forgotPasswordModel.forget(
-      hashPassword,
+      generateCode(),
       email,
       id
     );
+
     const newPassword = forgotPassword.rows[0];
     return res.json({
       success: true,
       message: "Create new password succesfully",
       results: newPassword,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: `Error: ${err.message}`,
+    });
+  }
+};
+
+exports.resetPassword = async (req, res) => {
+  try {
+    // get req.body (code, email. newPassword, confirmPassword)
+    // validation
+    // newPassword == confirmPassword
+    // sesuaikan code dengan yang ada di tabel forgotPassword
+    // update forgotPassword updatedAt untuk code hanya sekali dipakai (if updatedAt true => code cannot used)
+    // update password user
+    const { code, email, newPassword, confirmPassword } = req.body;
+    const getPassword = await forgotPasswordModel.getForgotPassword(code);
+
+    if (email != getPassword.rows[0].email) {
+      return res.status(400).json({
+        success: false,
+        message: "Email not match with Records Data",
+      });
+    }
+
+    if (newPassword != confirmPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Password not match with Confirm Password",
+      });
+    }
+
+    const updateUser = await userModel.updatePassword(
+      getPassword.rows[0].userId,
+      newPassword
+    );
+
+    return res.json({
+      success: true,
+      message: "User Password has been changed",
+      results: updateUser.rows[0],
     });
   } catch (err) {
     return res.status(500).json({
