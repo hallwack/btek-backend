5caf246fc33d6e43f4e8623809efe98b52035b02
Raihan Adam <hallwack.id@gmail.com>
2022-11-05 00:00:39 +0700

    chore: delete frontend

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const db = new Pool({
-  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:5432/${process.env.DB_DATABASE}?schema=public`
+  /* connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:5432/${process.env.DB_DATABASE}?schema=public`, */
+  connectionString: process.env.DB_URL,
 });
 
 module.exports = db;
